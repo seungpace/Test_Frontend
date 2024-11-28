c71850660ad4675cba0c4a98f80142e36cdd9b04
Svng <pedri008@naver.com>
2024-11-28 20:30:37 +0900

    Front for test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // CSS 파일을 import 합니다.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/users')
+            .then(response => response.json())
+            .then(data => {
+                setUsers(data);
+                console.log(data); // 콘솔에 데이터 출력
+            })
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>User Data</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>name</th>
+                    <th>email</th>
+                    <th>phoneNumber</th>
+                    <th>created-At</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map(user => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.username}</td>
+                        <td>{user.email}</td>
+                        <td>{user.phoneNumber}</td>
+                        <td>{new Date(user.createdAt).toLocaleString()}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
